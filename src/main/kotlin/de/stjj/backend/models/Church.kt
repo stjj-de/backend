208ac832043274517f35a1fe8a7bd919e42e78a8
Moritz Ruth <dev@moritzruth.de>
2020-07-30 13:34:15 +0200

    Use google maps ID instead of plus code

--- src/main/kotlin/de/stjj/backend/models/Church.kt
+++ src/main/kotlin/de/stjj/backend/models/Church.kt
@@ -11,31 +11,31 @@ import org.jetbrains.exposed.sql.statements.UpdateBuilder
 
 object Churches: IntIdTable("churches"), APIModel {
     val title = varchar("title", 255)
-    val plusCode = varchar("plus_code", 20).nullable()
+    val googleMapsID = varchar("google_maps_id", 255)
 
     override val defaultFields = "id,title"
     override val writePermissionChecker = APIModel.minimumRole(User.Role.EDITOR)
     override val apiFields = setOf(
             APIField.C("id", id, true),
             APIField.C("title", title, true),
-            APIField.C("plusCode", plusCode, false)
+            APIField.C("googleMapsID", googleMapsID, false)
     )
     override val buildWhereCondition = APIModel.createIntIDGetOneSelectExpression(Churches)
 
     override fun applyData(ctx: Context, it: UpdateBuilder<Int>, isUpdate: Boolean) {
         val data = ctx.body(CreateOrUpdateData::class.java)
         it[title] = data.title
-        it[plusCode] = if (data.plusCode.isBlank()) null else data.plusCode
+        it[googleMapsID] = data.googleMapsID
     }
 
     data class CreateOrUpdateData(
             val title: String,
-            val plusCode: String
+            val googleMapsID: String
     )
 }
 
 class Church(id: EntityID<Int>): IntEntity(id) {
     companion object : IntEntityClass<Church>(Churches)
     var title by Churches.title
-    var plusCode by Churches.plusCode
+    var googleMapsID by Churches.googleMapsID
 }
